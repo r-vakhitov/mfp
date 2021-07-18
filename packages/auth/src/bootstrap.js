0046dc31a0ab3f4930dae877ecb919d823d47ddc
Roman <outpacroman@gmail.com>
2021-07-18 17:32:48 +0300

    add routes

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory }) => {
   const history = defaultHistory || createMemoryHistory();
 
@@ -10,7 +11,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history}/>, el);
+  ReactDOM.render(<App history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -19,18 +20,20 @@ const mount = (el, { onNavigate, defaultHistory }) => {
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-    }
-  }
+    },
+  };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
-  // Check if exists
-  
   if (devRoot) {
-    mount(devRoot, { defaultHistory: createBrowserHistory() })
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
